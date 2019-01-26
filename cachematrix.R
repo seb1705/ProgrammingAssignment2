fdf22994ee2bd40e981329cbb911648ef5a4733f
Sujo Jose <sujo_jose@yahoo.com>
2019-01-26 08:11:25 -0600

    added function code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix 
+##   This function creates a special "matrix" object that can cache its inverse.
+## cacheSolve
+##   It computes the inverse of the special "matrix" returned by makeCacheMatrix 
+##   If the inverse has already been calculated (and the matrix has not changed), 
+##   then the cachesolve should retrieve the inverse from the cache.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function which creates special matrix object(x) 
+##   which cache its inverse (inv). 
+## Object returned by this function will have functions set and get anabling   
+##   set       store set new values to matrix 
+##   get       retrieve values stored matrix values
+##   setsolve  store inverse of the matrix in cache (inv)
+##   getsolve  retrieve cached inverse  
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y){
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) inv <<- solve
+    getsolve <- function() inv
+    list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function takes special matrix object as input and output its inverse
+##   it checks if  inverse of matrix is cached and returns from cache when present
+##   when inverse is not cached, it calculates inverse and store in cache for next time
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Check whether inverse of matrix is already cached, if yes return from cache
+    i <- x$getsolve()
+    if(!is.null(i)){
+        message("getting cached data")
+        return(i)
+    }
+    ## get the values stored in matrix x and calculate inverse using solve fun
+    message("calculating inverse")
+    m <- x$get()
+    i <- solve(m, ...)
+    ## Update cache with inverse and return calculated inverse
+    x$setsolve(i)
+    m
 }
